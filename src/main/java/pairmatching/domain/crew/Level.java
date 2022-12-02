9e8cd04c2663981d358e62c5347e2e3b5628a9b1
kimJaeHyeon <kmr2644@gmail.com>
2022-12-02 17:56:45 +0900

    feat: 레벨 enum 생성메서드 구현

--- src/main/java/pairmatching/domain/crew/Level.java
+++ src/main/java/pairmatching/domain/crew/Level.java
@@ -1,5 +1,7 @@
 package pairmatching.domain.crew;
 
+import java.util.Arrays;
+
 public enum Level {
     ONE("레벨1"),
     TWO("레벨2"),
@@ -8,8 +10,16 @@ public enum Level {
     FIVE("레벨5");
 
     private final String level;
+
     Level(String level) {
         this.level = level;
+    }
+
+    public static Level create(String inputLevel) {
+        return Arrays.stream(Level.values())
+                .filter(level -> level.name().equals(inputLevel))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
     }
 
     @Override
