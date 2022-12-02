4631fdac84a3d08beec4300e2e40b4bfda2db50e
kimJaeHyeon <kmr2644@gmail.com>
2022-12-02 16:39:00 +0900

    feat: Level 상수 enum 구현

--- src/main/java/pairmatching/domain/crew/Level.java
+++ src/main/java/pairmatching/domain/crew/Level.java
@@ -1,4 +1,19 @@
 package pairmatching.domain.crew;
 
 public enum Level {
+    ONE("레벨1"),
+    TWO("레벨2"),
+    THREE("레벨3"),
+    FOUR("레벨4"),
+    FIVE("레벨5");
+
+    private final String level;
+    Level(String level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return level;
+    }
 }
