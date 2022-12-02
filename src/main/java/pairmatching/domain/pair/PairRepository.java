7809ab77f809daa6480fe6510b62d70cffdac8d5
kimJaeHyeon <kmr2644@gmail.com>
2022-12-02 20:41:43 +0900

    feat: PairRepository 과정과 미션을 인자로받아 조회하는 메서드 구현

--- src/main/java/pairmatching/domain/pair/PairRepository.java
+++ src/main/java/pairmatching/domain/pair/PairRepository.java
@@ -2,11 +2,21 @@ package pairmatching.domain.pair;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import pairmatching.domain.crew.Course;
+import pairmatching.domain.crew.Mission;
 
 public class PairRepository {
     private final List<Pair> pairs = new ArrayList<>();
 
     public void save(final List<Pair> pairs) {
         this.pairs.addAll(pairs);
+    }
+
+    public List<Pair> findByCourseAndMission(Course course, Mission mission) {
+        return pairs.stream()
+                .filter(pair -> pair.isSameCourse(course))
+                .filter(pair -> pair.isSameMission(mission))
+                .collect(Collectors.toList());
     }
 }
