d0b452692662e32cbb38554c2cf2980c90cee66d
kimJaeHyeon <kmr2644@gmail.com>
2022-12-02 19:53:52 +0900

    feat: 크루의 인원에 따라 매칭로직을 추상화한 인터페이스 구현

---
+++ src/main/java/pairmatching/policy/PairMatchingPolicy.java
@@ -0,0 +1,14 @@
+package pairmatching.policy;
+
+import java.util.List;
+import pairmatching.domain.crew.Course;
+import pairmatching.domain.crew.Crew;
+import pairmatching.domain.crew.Mission;
+import pairmatching.domain.pair.Pair;
+
+public interface PairMatchingPolicy {
+
+    List<Pair> pairing(List<Crew> shuffledCrews, Course course, Mission mission);
+
+
+}
