839a2c69a56457b1bd4ff12b54b00c208b86c17f
kimJaeHyeon <kmr2644@gmail.com>
2022-12-02 17:20:02 +0900

    feat: equals hasCode 오버라이딩, 같은 과정인 확인하는 메서드 구현

--- src/main/java/pairmatching/domain/crew/Crew.java
+++ src/main/java/pairmatching/domain/crew/Crew.java
@@ -2,6 +2,7 @@ package pairmatching.domain.crew;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Crew {
     private final Course course;
@@ -18,5 +19,24 @@ public class Crew {
         return new Crew(course, name);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Crew crew = (Crew) o;
+        return course == crew.course && name.equals(crew.name);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(course, name);
+    }
+
+    public boolean isSameCourse(Course course) {
+        return this.course.equals(course);
+    }
 }
