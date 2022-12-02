b9941164758f298638795183688fc2ce9127385f
kimJaeHyeon <kmr2644@gmail.com>
2022-12-02 19:59:59 +0900

    refactor: pariMatchingDto 를받아 pairMatching 하는 메서드 구현

---
+++ src/main/java/pairmatching/service/PairMatchingService.java
@@ -0,0 +1,71 @@
+package pairmatching.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.Collectors;
+import pairmatching.domain.crew.Course;
+import pairmatching.domain.crew.Crew;
+import pairmatching.domain.crew.CrewRepository;
+import pairmatching.domain.crew.Mission;
+import pairmatching.domain.pair.Pair;
+import pairmatching.domain.pair.PairRepository;
+import pairmatching.dto.PairMatchingDto;
+import pairmatching.exception.ErrorCode;
+import pairmatching.policy.DefaultPairMatchingPolicy;
+import pairmatching.policy.OddCrewsPairMatchingPolicy;
+import pairmatching.policy.PairMatchingPolicy;
+import pairmatching.utils.PairUtils;
+
+public class PairMatchingService {
+    //쿼리
+    private final CrewRepository crewRepository;
+    private final PairRepository pairRepository;
+
+    public PairMatchingService(CrewRepository crewRepository, PairRepository pairRepository) {
+
+        this.crewRepository = crewRepository;
+
+        this.pairRepository = pairRepository;
+    }
+
+    public void pairMatching(PairMatchingDto pairMatchingDto) {
+        Course course = pairMatchingDto.getCourse();
+        List<Crew> crews = crewRepository.findByCourse(course);
+        Mission mission = pairMatchingDto.getMission();
+        List<Pair> pairs = tryPairing(crews, course, mission);
+        pairRepository.save(pairs.stream()
+                .map(Pair::addPaired)
+                .collect(Collectors.toList()));
+
+
+    }
+
+    private List<Pair> tryPairing(List<Crew> crews, Course course, Mission mission) {
+        int tryCount = 0;
+        while (PairUtils.isOverTryCount(tryCount)) {
+            List<Crew> shuffled = Randoms.shuffle(crews);
+            try {
+                return selectPolicyAndReturnPairs(shuffled, course, mission);
+            } catch (IllegalArgumentException e) {
+                tryCount++;
+            }
+        }
+        //리팩토링
+        throw new IllegalArgumentException(ErrorCode.FAILED_PAIR_MATCHING.getMessage());
+    }
+
+    private List<Pair> selectPolicyAndReturnPairs(List<Crew> crews, Course course, Mission mission) {
+        if (PairUtils.isOdd(crews)) {
+            return pairingCrews(crews, course, mission, new OddCrewsPairMatchingPolicy());
+        }
+        return pairingCrews(crews, course, mission, new DefaultPairMatchingPolicy());
+
+    }
+
+    private List<Pair> pairingCrews(List<Crew> crews, Course course, Mission mission,
+                                    PairMatchingPolicy pairMatchingPolicy) {
+        return pairMatchingPolicy.pairing(crews, course, mission);
+    }
+
+    //페어매칭 조회
+}
