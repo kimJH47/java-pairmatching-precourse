cd98a047e2d916a321012cc0dfe66423fd94a9b6
kimJaeHyeon <kmr2644@gmail.com>
2022-12-02 17:02:35 +0900

    feat: 미션 상수 enum 구현

---
+++ src/main/java/pairmatching/domain/crew/Mission.java
@@ -0,0 +1,30 @@
+package pairmatching.domain.crew;
+
+
+public enum Mission {
+
+    CAR_RACE(Level.ONE,"자동차경주"),
+    LOTTO(Level.ONE,"로또"),
+    BASEBALL(Level.ONE,"숫자야구게임"),
+    CART(Level.TWO, "장바구니"),
+    PAYMENT(Level.TWO, "결제"),
+    SUBWAY_MAP(Level.TWO, "지하철노선도"),
+    PERFORMANCE_IMPROVEMENT(Level.FOUR, "성능개선"),
+    RELEASE(Level.FOUR, "배포");
+
+
+    private final Level level;
+    private final String name;
+
+    Mission(Level level, String name) {
+        this.level = level;
+        this.name = name;
+    }
+
+    //생성메서드 구현
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
