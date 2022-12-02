314a602d723ee8990c3b36599ea8304c2b9b24b9
kimJaeHyeon <kmr2644@gmail.com>
2022-12-02 16:42:07 +0900

    feat: Course 상수 enum 구현 및 생성 메서드 구현

--- src/main/java/pairmatching/domain/crew/Course.java
+++ src/main/java/pairmatching/domain/crew/Course.java
@@ -1,4 +1,27 @@
 package pairmatching.domain.crew;
 
+import java.util.Arrays;
+
 public enum Course {
+    BACK_AND("백앤드"),
+    FRONT_AND("프론트앤드");
+
+    private final String courseName;
+
+    Course(String courseName) {
+
+        this.courseName = courseName;
+    }
+
+    public static Course create(String inputCourse) {
+        return Arrays.stream(Course.values())
+                .filter(course -> course.courseName.equals(inputCourse))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+
+
+
+
 }
