b6cc20d7ce54f5a2881225483dc1a604605494bc
kimJaeHyeon <kmr2644@gmail.com>
2022-12-02 17:20:42 +0900

    feat: 과정을 인자로 받아 크루전체를 조회하는 메서드 구현

--- src/main/java/pairmatching/domain/crew/CrewRepository.java
+++ src/main/java/pairmatching/domain/crew/CrewRepository.java
@@ -2,12 +2,11 @@ package pairmatching.domain.crew;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CrewRepository {
     private final List<Crew> crews = new ArrayList<>();
 
-    //등록
-    //중복확인
     public Crew save(final Crew crew) {
         validateDuplicate(crew);
         crews.add(crew);
@@ -19,5 +18,10 @@ public class CrewRepository {
             throw new IllegalArgumentException("크루이름이 중복됩니다");
         }
     }
-    //조회
+
+    public List<Crew> findByCourse(Course course) {
+        return crews.stream()
+                .filter(crew -> crew.isSameCourse(course))
+                .collect(Collectors.toList());
+    }
 }
